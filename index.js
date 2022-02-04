46b4bc9b33554d98f911b884e24b6a2b4dd8b2f9
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-04 17:08:04 +0500

    started otlichno

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 const telegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+const path = require("path");
 
-const bot = new telegramBot("5215622865:AAE118t9hmnZDWZG_tLVCt__XC5DJZONRHw", {
+const bot = new telegramBot("5173334455:AAF2gZChStHU2aYoJJakH-NHt1UCZDxQq6I", {
   polling: true,
 });
 
 bot.onText(/\/start/, (message) => {
-  console.log(message);
-
   const chatId = message.chat.id;
   bot.sendMessage(
     chatId,
-    `Salom ${message.from.first_name} "Abduhalim" yaratgan botga xush kelibsiz!`
+    `Salom ${message.from.first_name} "Oтличнo" o'quv markazi botiga xush kelibsiz!`,
+    {
+      reply_markup: JSON.stringify({
+        keyboard: [
+          //! alohida alohida arrayni ichida yozsak pastma past chiqaradi agar bitta arrayni ichiga yozsak yonma yon chiqaradi
+          [
+            {
+              text: "📚 Kurs haqida ma`lumot",
+            },
+            {
+              text: "👨‍🎓 kursga yozilish",
+            },
+          ],
+        ],
+        resize_keyboard: true, //--> bu chiqarvotgan buttonni xajmini ixchamlashtiradi
+      }),
+    }
   );
+});
+
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
+//   if (msg.text == "📚 Kurs haqida ma`lumot") {
+//     reply_markup: JSON.stringify({
+//       keyboard: [
+//         [
+//           {
+//             text: "O`quv markazimizdagi Kurslarimiz faqat Rus tiliga yo`naltirilgan bo`lib Buning qulayliklari: \n 1: Ayollar uchun alohida kurslarimiz, \n 2: Bolalar uchun alohida kurslarimiz, \n 3: Kattalar uchun alohida kurslarimiz, \n 4: Online xolatda xam kurslarimiz mavjud",
+//           },
+//         ],
+//       ],
+//     });
+//   }
+// });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text == "📚 Kurs haqida ma`lumot") {
+    bot.sendPhoto(
+      chatId,
+      fs.readFileSync(path.resolve(__dirname, "./images/kurs.jpg")),
+      {
+        parse_mode: "HTML",
+        caption: `
+                <strong>
+                  O'quv markazimizdagi Kurslarimiz faqat Rus tiliga yo'naltirilgan bo'lib Buning qulayliklari: \n 1: Ayollar uchun alohida kurslarimiz, \n 2: Bolalar uchun alohida kurslarimiz, \n 3: Kattalar uchun alohida kurslarimiz, \n 4: Online xolatda xam kurslarimiz mavjud
+                </strong>\n<a href="https://sendpulse.com/support/glossary/social-media-marketing">Media havola SMM</a>
+            `,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Kursga yozilish",
+                callback_data: "smm_course_register",
+              },
+            ],
+          ],
+        },
+      }
+    );
+  }
 });
